c23a4957ca4ac8b1e49814d5bccbdd011e013dea
shinscript <edu.markshin@gmail.com>
2023-04-04 14:11:54 -0400

    fetch test

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import vcrData from "../public/data/data.json" assert { type: "json" };
+
 const yearSelectionDiv = document.querySelector(".year-selections");
 
 const decades = ["1960s", "1970s", "1980s", "1990s", "2000s", "2010s"];
