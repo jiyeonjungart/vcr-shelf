2c0de381ac7534763075885e7e6fc9b97f38bcf1
shinscript <edu.markshin@gmail.com>
2023-04-04 14:02:10 -0400

    finished header

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const yearSelectionDiv = document.querySelector(".year-selections");
+
+const decades = ["1960s", "1970s", "1980s", "1990s", "2000s", "2010s"];
+
+const scroller = document.querySelector(".year-selections");
+
+window.addEventListener("wheel", (e) => {
+  scroller.scrollLeft += e.deltaY;
+});
+
+decades.forEach((year) => {
+  const yearsElement = document.createElement("div");
+  yearsElement.className = "years";
+  yearsElement.innerText = year;
+  yearSelectionDiv.appendChild(yearsElement);
+});
+
+// ================== MAIN ====================
